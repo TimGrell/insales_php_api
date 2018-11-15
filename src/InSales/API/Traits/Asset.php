32f88838f55a95041b2ad137c98e745f230f5d56
Tim Hell <timhell64@gmail.com>
2018-11-15 20:11:28 +0700

    Исправлена опечатка

--- src/InSales/API/Traits/Asset.php
+++ src/InSales/API/Traits/Asset.php
@@ -42,7 +42,7 @@ trait Asset
      * @param array $data Массив данных
      * @return ApiResponse
      */
-    public function updateArticle(int $id, int $themeId, array $data) : ApiResponse
+    public function updateAsset(int $id, int $themeId, array $data) : ApiResponse
     {
         return $this->client->executeUpdateRequest(
             $this->generateCompoundUrl(self::API_URL_ARTICLE, $themeId, $id),
